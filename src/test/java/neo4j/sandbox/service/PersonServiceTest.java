88a878af18fbf56c71b852f29450be5ca413dbf4
Jack <jrmpatwork@gmail.com>
2012-03-20 09:51:57 +0000

    remove old import

--- src/test/java/neo4j/sandbox/service/PersonServiceTest.java
+++ src/test/java/neo4j/sandbox/service/PersonServiceTest.java
@@ -8,7 +8,6 @@ import neo4j.sandbox.model.Person;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.stereotype.Service;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
